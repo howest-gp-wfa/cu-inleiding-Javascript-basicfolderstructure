041967a8c40f115d7159c293c1e0614adbd6760b
Mileto <mileto.di.marco@howest.be>
2020-02-05 11:34:56 +0100

    add script basic structure

--- js/script.js
+++ js/script.js
@@ -1,42 +1,13 @@
 "use strict";
 
+//declare global vars here
+
+//wait for document load
+window.addEventListener('load',Initialize);
 
 function Initialize()
 {
-    const pi = 3.14;
-    var getal1 = 10;
-    let getal2 = 12;
-}
-
-
-function ToonGetallen()
-{
-   // console.log("Getal1="+getal1);
-    //console.log("Getal2="+getal2);
-   // console.log("pi="+pi);
-}
-
-let aantalHerhalingen = 0;
-let getal = 33
-let randomGetal = Math.floor(Math.random()*100);
-while(getal !=randomGetal)
-{
-    aantalHerhalingen++;
-    randomGetal = Math.floor(Math.random()*100);
-    console.log(randomGetal);
-}
-console.log("aantal herhalingen="+aantalHerhalingen);
-
-//test the map function
-var ar_getallen = [21,45,7,8,65,9,98,65,32,2];
-var evenNumbers = ar_getallen.map(function{});
-evenNumbers.forEach(element => {console.log(element);
     
-});
+}
 
 
-
-Initialize();
-ToonGetallen();
-
-//console.log("getal1="+getal1);
